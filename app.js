a48792a55aca55f5262c19a2e65ef1d710eb409b
Carlangasv <carloszzzxc@hotmail.com>
2020-04-07 16:05:36 -0500

    Registros, app
    Se crea el archivo app.js, este define el punto de entrada para el api y nos sirve para delimitar los endpoints, tambien se inicia con la implementacion del archivo registros

---
+++ app.js
@@ -0,0 +1,24 @@
+//Importamos express
+const express = require("express");
+
+//Se inicializa la libreria
+const app = express();
+app.use(express.json());
+
+//Se crea el endpoint (Este delimita hasta donde va el api)
+/**
+ * URI's  disponibles en el API
+ */
+app.get("/", (req, res) => {
+  res.send("Bienvenido a la API guardado de urls");
+});
+// IMPORTAR las rutas con los endpoints especificos
+const rutas_registros = require("./routes/registros");
+app.use(rutas_registros);
+
+// Puerto
+const port = 3000;
+// Levantar el servidor para escuchar los puertos
+app.listen(port, () => {
+  console.log(`Escuchando API en http://localhost:${port}`);
+});
