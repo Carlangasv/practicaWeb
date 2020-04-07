8990688fd33126e0091bb376514f60d4591e9310
Carlangasv <carloszzzxc@hotmail.com>
2020-04-07 15:51:17 -0500

    Postgres
    Se crea el servicio de postgres

---
+++ services/postgres.js
@@ -0,0 +1,25 @@
+//Importamos la dependecia de postgres
+const { Pool } = require("pg");
+
+class ServicioPG {
+  constructor() {
+    this.pool = new Pool({
+      user: "postgres",
+      host: "localhost",
+      database: "Url",
+      password: "",
+      port: 5432
+    });
+}
+/**
+ * Se ejecuta una consulta sql, se debe hacer con un metodo
+ * asincrono ya que el orden de ejecucion es relevante
+ * @param {*} sql la consulta a ejecutar
+ */
+  async ejecutarSql(sql) {
+    let respuesta = await this.pool.query(sql);
+    return respuesta;
+  }
+}
+//Se exporta la clase para que pueda ser usada en todo el proyecto
+module.exports = ServicioPG;
